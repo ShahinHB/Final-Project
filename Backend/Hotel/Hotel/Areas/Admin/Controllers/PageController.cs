179e1efb4b286fffe739c5fc96d0fc4af57c9180
ShahinHB <72246220+ShahinHB@users.noreply.github.com>
2022-02-18 16:15:14 +0400

    Change Admin Panel Template

---
+++ Backend/Hotel/Hotel/Areas/Admin/Controllers/PageController.cs
@@ -0,0 +1,114 @@
+﻿using Hotel.Data;
+using Hotel.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class PageController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PageController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Create(Page model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.ImageFile.ContentType == "image/jpeg" || model.ImageFile.ContentType == "image/png")
+                {
+                    string fileName = model.Title;
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        model.ImageFile.CopyTo(stream);
+                    }
+
+                    model.ImageName = fileName;
+                    _context.Pages.Add(model);
+                    _context.SaveChanges();
+
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "You can upload only .jpeg, .jpg and .png");
+                    return View(model);
+                }
+            }
+            ModelState.AddModelError("", "All section is required");
+            return View(model);
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(_context.Pages.Find(id));
+        }
+        
+        [HttpPost]
+        public IActionResult Update(Page model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.ImageFile.ContentType == "image/jpeg" || model.ImageFile.ContentType == "image/png")
+                {
+                    string fileName = model.Title + DateTime.Now;
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        model.ImageFile.CopyTo(stream);
+                    }
+
+                    model.ImageName = fileName;
+                    _context.Pages.Update(model);
+                    _context.SaveChanges();
+
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "You can upload only .jpeg, .jpg and .png");
+                    return View(model);
+                }
+            }
+            ModelState.AddModelError("", "All section is required");
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Page page = _context.Pages.Find(id);
+            _context.Pages.Remove(page);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
